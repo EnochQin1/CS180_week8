280fcd64fc8fa7551fccc2f141235da53a546ce8
EnochQin1 <enoch1944@gmail.com>
2021-10-18 16:19:04 -0400

    monday notes

--- src/main/java/com/example/cs180/Week9/Notes.java
+++ src/main/java/com/example/cs180/Week9/Notes.java
@@ -3,6 +3,26 @@ package com.example.cs180.Week9;
 public class Notes
 {
     /*
-    
+    Java Threads
+    Thread class with run() method
+    import java.lang.*;
+    - Allows creation and manipulation of threads
+        - Thread t = new Thread();
+        - t.start(): start the thread referenced by t
+        - t.join(): "join with" (wait for) the running thread t
+        - t.run(): called by start() in a different thread
+    -
      */
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        System.out.printf("main thread=%s\n", t);
+
+        System.out.printf("going to sleep...\n");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("ah, that was nice");
+    }
 }
