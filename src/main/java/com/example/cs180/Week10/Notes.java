f7265c49ea9d06b2aefe5f2a221b1a07431a3758
EnochQin1 <enoch1944@gmail.com>
2021-10-25 16:37:46 -0400

    Week10 notes

---
+++ src/main/java/com/example/cs180/Week10/Notes.java
@@ -0,0 +1,34 @@
+package com.example.cs180.Week10;
+
+public class Notes {
+    /*
+    Network I/O
+        - Internet Protocol (IP)
+        Identifies hosts with a unique address
+        - Domain Name System (DNS)
+        Maps domain names to IP addresses
+        - Transmission Control Protocol (TCP)
+        identifies ports on hosts for a network connection
+        - Socket
+        IP address plus TCP port
+        two sockets makes a network connection
+
+        Client-Server
+        - A server is a process that waits for a connection
+        - A client is a process that connects to a server
+        - At different times, a process may be both a client and a server
+        - Need not be associated with a specific computer: any computer
+        can have both client and server processes running on it
+        - Once connected, the client and server can both read and write data to
+        one another asynchronously
+        Clients and Server communicate via Sockets
+        Socket: IP address plus TCP port
+        street name + house number
+        IP address - byte notation, unique, computer on the internet, some firewalls translate addresses
+        to internal ones
+
+        Java objects can be written to and read from files
+        exchanged over network connections
+
+     */
+}
