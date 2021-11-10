e46fa78ee3bae79a5f64d938dbc2f09624b0133c
EnochQin1 <enoch1944@gmail.com>
2021-11-10 15:01:19 -0500

    Project work

--- src/main/java/com/example/cs180/Project4/DiscussionPost.java
+++ src/main/java/com/example/cs180/Project4/DiscussionPost.java
@@ -4,45 +4,86 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
 
 public class DiscussionPost {
     private String topic;
     private String prompt;
-    private char firstLetter;
     private int votes;
     private ArrayList<Reply> replies;
+
     public DiscussionPost(String topic, String prompt) {
         this.topic = topic;
         this.prompt = prompt;
         this.votes = 0;
         this.replies = new ArrayList<Reply>();
-        this.firstLetter = topic.charAt(0);
     }
+
     public void addVote() {
         this.votes++;
     }
+
     public int getVotes() {
         return this.votes;
     }
+
     public void addReply(Reply reply) {
         this.replies.add(reply);
     }
-    public ArrayList<Reply> getReplies() { return this.replies; }
+
+    public ArrayList<Reply> getReplies() {
+        return this.replies;
+    }
 
     public class VoteComparator implements Comparator<Reply> {
         public int compare(Reply r1, Reply r2) {
             return r2.getVotes() - r1.getVotes();
         }
     }
+
     public void sortRepliesByVotes() {
         Collections.sort(replies, new VoteComparator());
     }
-    public String getTopic() { return this.topic; }
-    public String getPrompt() { return this.prompt; }
-    public int numReplies() { return this.replies.size(); }
+
+    public class TimeComparator implements Comparator<Reply> {
+        public int compare(Reply r1, Reply r2) {
+            int result = r2.getTimeCreated().compareTo(r1.getTimeCreated());
+            return result;
+        }
+    }
+    public void sortRepliesByTime() {
+        Collections.sort(replies, new TimeComparator());
+        ArrayList<Reply> finalList = new ArrayList<>();
+        //for ( int i = replies.size() - 1; i >= 0; i--) {
+            //finalList.add(replies.get(i));
+        //}
+        //replies = finalList;
+    }
+    public String getTopic() {
+        return this.topic;
+    }
+
+    public String getPrompt() {
+        return this.prompt;
+    }
+
+    public int numReplies() {
+        return this.replies.size();
+    }
+
     public static void main(String[] args) {
-        Reply test1 = new Reply("1");
         Reply test2 = new Reply("2");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Reply test1 = new Reply("1");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Reply test3 = new Reply("3");
         test3.addVote();
         test3.addVote();
@@ -51,13 +92,13 @@ public class DiscussionPost {
         test1.addVote();
         test1.addVote();
         test1.addVote();
-        DiscussionPost disTest = new DiscussionPost("","");
-        disTest.addReply(test1);
+        DiscussionPost disTest = new DiscussionPost("test", "test");
         disTest.addReply(test2);
+        disTest.addReply(test1);
         disTest.addReply(test3);
-        disTest.sortRepliesByVotes();
-        for( int i = 0; i < disTest.numReplies(); i++) {
-            System.out.println(disTest.getReplies().get(i).getMessage());
+        disTest.sortRepliesByTime();
+        for (int i = 0; i < disTest.replies.size(); i++) {
+            System.out.println(disTest.replies.get(i).getMessage());
         }
     }
 }
--- src/main/java/com/example/cs180/Project4/Reply.java
+++ src/main/java/com/example/cs180/Project4/Reply.java
@@ -1,11 +1,15 @@
 package com.example.cs180.Project4;
 
+import java.time.LocalTime;
+
 public class Reply {
     private int votes;
     private String message;
+    private LocalTime timeCreated;
     public Reply(String message) {
         this.message = message;
         this.votes = 0;
+        this.timeCreated = java.time.LocalTime.now();
     }
     public void addVote() {
         this.votes++;
@@ -16,5 +20,9 @@ public class Reply {
     public String getMessage() {
         return this.message;
     }
-
+    public LocalTime getTimeCreated() { return timeCreated; }
+    public static void main(String[] args) {
+        Reply test = new Reply("testing");
+        System.out.println(test.getTimeCreated());
+    }
 }
